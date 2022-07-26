1277bd1e3d5cd26a86c159e24a222098de24eb2d
VedantWankhade <vedantwankhade17@gmail.com>
2022-07-26 13:56:01 +0530

    Add digits in integer

--- src/main/java/general/elementary/AddDigitsInInteger.java
+++ src/main/java/general/elementary/AddDigitsInInteger.java
@@ -2,6 +2,7 @@ package general.elementary;
 
 /**
  * @author VedantWankhade
+ *
  * Problem: (Add the digits in an integer) Write a program that reads an integer between
  * 0 and 1000 and adds all the digits in the integer. For example, if an integer
  * is 932, the addition of all its digits is 14.
