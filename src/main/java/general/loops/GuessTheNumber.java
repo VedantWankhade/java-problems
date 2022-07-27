9fe3766d5c2465d70b3f7f3af135c8db9fe7b720
VedantWankhade <vedantwankhade17@gmail.com>
2022-07-27 13:47:50 +0530

    Guess the number

--- src/main/java/general/loops/GuessTheNumber.java
+++ src/main/java/general/loops/GuessTheNumber.java
@@ -1,2 +1,33 @@
-package general.loops;public class GuessTheNumber {
+package general.loops;
+
+import java.util.Scanner;
+
+/**
+ * You will write a program that
+ * randomly generates an integer between 0 and 100, inclusive. The program prompts the user
+ * to enter a number continuously until the number matches the randomly generated number. For
+ * each user input, the program tells the user whether the input is too low or too high, so the user
+ * can make the next guess intelligently
+ */
+public class GuessTheNumber {
+    public static void game() {
+        int num = (int) (Math.random() * 101); // random (0, 101]
+        int guess;
+
+        Scanner in = new Scanner(System.in);
+
+            System.out.println("Guess the number");
+        while (true) {
+            guess = in.nextInt();
+
+            if (guess == num) break;
+
+            System.out.println("your guess is too " + (guess > num ? "high" : "low"));
+        }
+        System.out.println("CORRECT");
+    }
+
+    public static void main(String[] args) {
+        game();
+    }
 }
